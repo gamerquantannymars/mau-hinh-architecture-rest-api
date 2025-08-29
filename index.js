b8dd4f67385811b9b30e2ce14e43f36a67706dbb
gamerquantannymars <oshiieyov@hotmail.com>
2025-08-29 05:34:09 +0300

    Add error handling for MongoDB connection

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ app.use(bodyParser.json());
 mongoose.connect('mongodb://localhost:27017/sanpham', {
   useNewUrlParser: true,
   useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error('Could not connect to MongoDB:', err);
+  process.exit(1);
 });
 
 // Mô hình sản phẩm
